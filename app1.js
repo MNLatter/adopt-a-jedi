100ec94763494cdf510dd22d0c8f2dac21d57f9f
Michael Latter <mlatter2@gmail.com>
2021-11-17 17:22:10 -0800

    removed folder

---
+++ app1.js
@@ -0,0 +1,9 @@
+let userName = prompt('Please Enter your Name!');
+
+console.log(userName)
+            'Welcome ' + 'Michael'
+            if(userName == 'Michael'){
+                document.write('Welcome Jedi Knight ' + userName)
+            } else {
+                document.write('Welcome ' + userName)
+            }
---
+++ app2.js
@@ -0,0 +1,5 @@
+let userAnswer = prompt('Do you want to see a secret message? (yes or no)');
+
+if(userAnswer.toUpperCase() == "YES"){
+    document.write('For my ally is the Force, and a powerful ally it is!!')
+}
